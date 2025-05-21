6c287ed4ccd2c7a09bdb2d5fb087efc0f3332760
Huy Vu <huyvu5412@gmail.com>
2025-05-21 23:43:33 +0700

    autopep8 fix

--- dags/model_utils/model_deployment.py
+++ dags/model_utils/model_deployment.py
@@ -7,6 +7,7 @@ import logging
 
 USER = os.getenv('USER')
 
+
 def register_model(run_id: str, model_name: str, tags: dict):
     # mlflow.set_tracking_uri(f"file:/home/{USER}/airflow/mlruns")
     # mlflow.set_tracking_uri("file:/opt/airflow/mlruns")
@@ -22,7 +23,6 @@ def register_model(run_id: str, model_name: str, tags: dict):
         client.set_model_version_tag(name=model_name, version=result.version, key=key, value=value)
 
     return result.version
-
 
 
 def compare_models(new_run_id: str, experiment_name: str, metric_key: str = "mape") -> bool:
@@ -42,7 +42,7 @@ def compare_models(new_run_id: str, experiment_name: str, metric_key: str = "map
 
     # Lấy top model trước đó trong cùng experiment
     experiment = client.get_experiment_by_name(experiment_name)
-    all_runs = client.search_runs(  
+    all_runs = client.search_runs(
         experiment_ids=[experiment.experiment_id],
         filter_string="tags.phase = 'validating'",
         order_by=[f"metrics.{metric_key} ASC"],
@@ -61,5 +61,5 @@ def compare_models(new_run_id: str, experiment_name: str, metric_key: str = "map
     best_old_model_uri = f"runs:/{best_old_run.info.run_id}/LSTM"
     print(f"[Old model] {metric_key}: {best_old_value:.4f}")
     print(f"[New model] {metric_key}: {new_metric_value:.4f}")
-    
+
     return new_metric_value < best_old_value, best_old_run.info.run_id
--- dags/model_utils/model_engineering.py
+++ dags/model_utils/model_engineering.py
@@ -68,7 +68,7 @@ def train_model(
     # mlflow.set_tracking_uri(f"file:/home/{USER}/airflow/mlruns")
     # mlflow.set_tracking_uri("file:/opt/airflow/mlruns")
     mlflow.set_tracking_uri("http://mlflow:5000")
-    
+
     print(mlflow.get_tracking_uri())
     # mlflow.set_tracking_uri("http://127.0.0.1:5000")
 
