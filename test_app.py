f1cc01be0d4f9fb6ba652f0a94bd0e31ecada981
Huy Vu <huyvu5412@gmail.com>
2025-05-23 14:36:20 +0700

    added pytest configs

--- test_app.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from fastapi_app.main import app  # your FastAPI instance
-
-client = TestClient(app)
-
-
-def test_home():
-    response = client.get("/")
-    assert response.status_code == 200
