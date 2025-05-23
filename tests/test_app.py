b2d2d704dd22e30148d5d48e5747b676ee77c21a
Huy Vu <huyvu5412@gmail.com>
2025-05-23 15:07:23 +0700

    changed dockerhub secrets

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,11 @@
 from fastapi.testclient import TestClient
 from fastapi_app.main import app  # your FastAPI instance
+import logging
 
 client = TestClient(app)
 
 
 def test_home():
+    logging.info("Pinging FastAPI server.")
     response = client.get("/")
     assert response.status_code == 200
